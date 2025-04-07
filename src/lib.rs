409a062088cfba1c03f2dd6a079775e782d44445
Felix Wiegand <fxwiegand@wgdnet.de>
2025-04-07 13:27:16 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,7 @@ extern crate curl_sys;
 extern crate libdeflate_sys;
 extern crate libz_sys;
 #[cfg(feature = "lzma")]
-extern crate lzma_sys;
+extern crate liblzma_sys;
 
 // include on-the-fly generated bindings
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
